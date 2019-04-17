9102cf2fb88e94f3b27b5f963062841292254a48
patrick9263 <psmith9263@gmail.com>
2019-04-17 00:41:06 -0400

    add placeholder button method

--- index.js
+++ index.js
@@ -4,31 +4,12 @@ const { shell } = require("electron");
 let app = new Vue({
   el: "#app",
   data: {
-    output:"Output text...",
-    clickCount: 0
+    output:"Output text..."
   },
 
   methods: {
-    admin: function(batchFunction) {
-
-      let bat = spawn("cmd.exe", arguments);
-
-      bat.stdout.on("data", data => {
-        this.output += data.toString();
-      });
-
-      bat.stderr.on("data", data => {
-        this.output += data.toString();
-      });
-
-      bat.on("exit", code => {
-        console.log(`Child exited with code ${code}`);
-      });
-      this.$refs.output.scrollTop += 400;
-    },
-
-    clearConsole: function() {
-      this.output = "";
+    calculate: function() {
+        console.log("Calcluating results...");
     }
   }
 });
