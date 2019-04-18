598e4a3c4a27003ff3a496ce0c3d120a70ebe65a
patrick9263 <psmith9263@gmail.com>
2019-04-17 21:10:10 -0400

    Maybe m-m-1 works

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { spawn } = require("child_process");
-const { shell } = require("electron");
 
 let app = new Vue({
   el: "#app",
@@ -8,8 +6,57 @@ let app = new Vue({
   },
 
   methods: {
+
+    setResult: function(elementID, text) {
+      document.getElementById(elementID).innerHTML = "= " + text;
+    },
+
     calculate: function() {
-        console.log("Calcluating results...");
+
+      // Grab the parameters from the text fields
+      document.getElementById("paramError").innerHTML = "";
+      var lambda = document.getElementById("lambda").value;
+      var mu = document.getElementById("mu").value;
+      var rho = document.getElementById("rho").value;
+
+      // If we have rho, we don't need anything else...
+      if (rho) {
+        var P0 = 1 - rho;
+        var Es = 1 / rho;
+        var RT = Es / P0;
+        var Lq = Math.pow(rho, 2) / P0;
+        var L = rho / P0;
+        var WT = (Es * rho) / P0;
+
+        this.setResult("Lresult", (1 / rho));
+        this.setResult("LqResult", Lq);
+        this.setResult("RTresult", RT);
+        this.setResult("WTresult", WT);
+      }
+
+      // If not, we need BOTH lambda and mu
+      else if (lambda && mu) {
+        var rho = lambda / mu;
+        var P0 = 1 - rho;
+        var Es = 1 / rho;
+        var RT = Es / P0;
+        var Lq = Math.pow(rho, 2) / P0;
+        var L = rho / P0;
+        var WT = (Es * rho) / P0;
+
+        this.setResult("Lresult", (1 / rho));
+        this.setResult("LqResult", Lq);
+        this.setResult("RTresult", RT);
+        this.setResult("WTresult", WT);
+      }
+
+      // Otherwise yell at the user
+      else {
+        document.getElementById("paramError").innerHTML = 
+          "<b>Must supply rho, or both lambda AND mu</b>";
+      }
     }
+
   }
+
 });
